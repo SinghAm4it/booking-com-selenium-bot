73489977873c72782cfc2e9123b9a36aaaac556f
SinghAm4it <125212729+SinghAm4it@users.noreply.github.com>
2025-10-30 13:44:07 +0530

    Add files via upload

---
+++ BookingsBot/constants.py
@@ -0,0 +1,86 @@
+RED = "\033[91m"
+
+BOLD = "\033[1m"
+
+RESET = "\033[0m"
+
+BASE_URL = "https://www.booking.com"
+
+CURRENCIES = {'INR','ARS','€$£', 'BGN', 'KZT', 'KWD',
+              'NOK', 'SEK', 'IDR', 'CAD', 'RUB', 
+              'COP', 'SAR', 'CLP', 'TRY', 'ZAR', 
+              'TWD', 'FJD', 'GBP', 'BRL', 'AED', 
+              'ILS', 'QAR', 'EGP', 'GEL', 'MDL', 'DKK', 
+              'THB', 'MXN', 'UAH', 'RON', 'XOF', 'AUD', 'CNY', 
+              'MOP', 'JPY', 'KRW', 'CHF', 'HUF', 'SGD', 'MYR', 
+              'ISK', 'EUR', 'CZK', 'BHD', 'USD', 'NZD', 'HKD', 
+              'JOD', 'OMR', 'PLN', 'AZN', 'NAD'}
+
+DATE_FLEXIBILITY = ["Exact dates","1 day","2 days","3 days","7 days"]
+
+STAY_DURATION = ["A weekend", "A week", "A month","Other"]
+
+FILTERS = [
+    "Restaurant",
+    "Room service",
+    "24-hour front desk",
+    "Fitness center",
+    "Non-smoking rooms",
+    "Airport shuttle",
+    "Family rooms",
+    "Spa",
+    "Hot tub/Jacuzzi",
+    "Free Wifi",
+    "Electric vehicle charging station",
+    "Wheelchair accessible",
+    "Kitchen facilities",
+    "Breakfast included",
+    "All meals included",
+    "All-inclusive",
+    "Breakfast & dinner included",
+    "Entire homes & apartments",
+    "Family-Friendly Properties",
+    "Villas",
+    "Apartments",
+    "Hotels",
+    "Guesthouses",
+    "Resorts",
+    "Vacation Homes",
+    "Hostels",
+    "Homestays",
+    "Swimming pool",
+    "Heated pool",
+    "Indoor pool",
+    "Outdoor pool",
+    "Private pool",
+    "Infinity Pool",
+    "Kids' pool",
+    "Shallow end",
+    "Wonderful: 9+",
+    "Very Good: 8+",
+    "Good: 7+",
+    "Pleasant: 6+",
+    "1 star",
+    "2 stars",
+    "3 stars",
+    "4 stars",
+    "5 stars",
+    "Free cancellation",
+    "Book without credit card",
+    "No prepayment",
+    "Twin beds",
+    "Double bed",
+    "Cribs",
+]
+
+SORT_LIST = [
+    "Top picks for long stays",
+    "Homes & apartments first",
+    "Price (lowest first)",
+    "Price (highest first)",
+    "Best reviewed & lowest price",
+    "Property rating (high to low)",
+    "Property rating (low to high)",
+    "Property rating and price",
+    "Top reviewed"
+]
